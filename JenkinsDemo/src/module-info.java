7f64d621e2d6f5822fb1769f882a1e9644f3da37
Sashi Saladi <sasi7816@gmail.com>
2023-07-09 15:05:09 -0700

    jenkinsdemo commit

---
+++ JenkinsDemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author padma
+ *
+ */
+module JenkinsDemo {
+}
---
+++ JenkinsDemo/src/net/codejava/HelloWorld.java
@@ -0,0 +1,11 @@
+package net.codejava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a demo code for Jenkins and github integration");
+
+	}
+
+}
