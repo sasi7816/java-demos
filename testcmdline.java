217d5d95e8cc491cc6e8386d4b5efe51ba9e0c4c
Sashi Saladi <sasi7816@gmail.com>
2023-07-09 21:20:27 -0700

    add testcmdline.java file

---
+++ testcmdline.java
@@ -0,0 +1,8 @@
+public class testcmdline {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("this code is a jenkins demo build");
+	}
+
+}
