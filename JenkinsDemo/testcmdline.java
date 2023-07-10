2aa8abdc99c7d1a64d00d70c892c8102b820a554
Sashi Saladi <sasi7816@gmail.com>
2023-07-09 21:18:17 -0700

    remove jenkins demo

--- JenkinsDemo/testcmdline.java
+++
@@ -1,8 +0,0 @@
-public class testcmdline {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-         System.out.println("this code is a jenkins demo build");
-	}
-
-}
